c2f38168026c5b46ed1f7827d18ab0664b78de46
Teja H R <tejahr6993@gmail.com>
2024-02-27 19:04:39 +0530

    Responsive Video.

--- src/Utils/Vedio.js
+++ src/Utils/Vedio.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactPlayer from "react-player";
-// import "../styling/videoplayer.scss"
+import "../styling/videoplayer.scss"
 
 const VedioPlayerComp = ({ vedioParams = {} }) => {
   return (
     <div className="videoplayer-container">
-      <ReactPlayer url={vedioParams.url}/>
+      <ReactPlayer url={vedioParams.url} className="react-player"/>
     </div>
   );
 };
